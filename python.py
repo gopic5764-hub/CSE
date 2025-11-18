7cf87702ce7be341ae066fdb4cb7bd22f07c257b
gopic5764-hub <gopic5764@gmail.com>
2025-11-18 12:18:02 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,34 @@
+# class laptop:
+#     # attributes
+#     brand,price="Dell",50000
+#     # methods
+#     def print_ing(self):
+#         print(f"{self.brand} and {self.price}")
+# lp1=laptop()
+# # print(lp1.brand)
+# # print(lp1.price)
+# lp1.print_ing()
+
+# create a class book with atributes title and author and the 
+# method details() and print both values
+
+class book:
+    # attributes
+    title,author="SSMB29","RAJAMOULI"
+    # methods
+    def a(py):
+        print(f"{py.title} and {py.author}")
+b=book()
+print(b.title)
+print(b.author)
+b.a()
+
+
+
+
+
+
+
+
+
+
